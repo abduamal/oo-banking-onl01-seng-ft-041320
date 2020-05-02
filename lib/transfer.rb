0f87ed5284f84e1f23ce513e5516e64f54e6e92c
Amal Abdu <abduamal@ymail.com>
2020-05-02 16:04:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,11 +2,5 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
   attr_reader :transfer,
 
-  def initialize(transfer, status = "pending", amount = 50 )
-    @transfer = transfer
-    @sender = sender
-    @receiver = receiver
 
-
-  end
 end
