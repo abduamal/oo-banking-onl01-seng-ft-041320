24157618360b9457e30e21984a92a4b10af8ff81
Amal Abdu <abduamal@ymail.com>
2020-05-02 16:11:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,8 +2,8 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
   attr_reader :transfer
 
-  def initialize(transfer, sender, receiver, amount = 50)
-    transfer = Transfer.new(sender, receiver, amount)
+  def initialize(transfer, sender, receiver, amount)
+    transfer = Transfer.new(sender, receiver, 50)
 
   end
 
