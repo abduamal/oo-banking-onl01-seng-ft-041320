6eea1a85f98d9718fb39b3b42c7fa402ac412fe5
Amal Abdu <abduamal@ymail.com>
2020-05-02 13:51:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,12 @@
 class BankAccount
   attr_reader :bank_account, :name
-  attr_accessor :balance, 
+  attr_accessor :balance, :status
 
   def initialize(name)
     @bank_account = bank_account
     @name = name
+    @balance = 1000
+    @status = "open"
   end
 
 end
