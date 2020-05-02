1618de7260189446073b663d33b206f960d5b3f7
Amal Abdu <abduamal@ymail.com>
2020-05-02 14:59:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ class Transfer
 
   def initialize(transfer, sender, receiver)
     @transfer = transfer
-    @sender = ""
-    @receiver = receiver
+    @sender = amanada
+    @receiver = 
   end
 end
