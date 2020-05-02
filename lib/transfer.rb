0902940587eff5821dccf172455ebac10a49652d
Amal Abdu <abduamal@ymail.com>
2020-05-02 16:36:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
+    
   end
+
 end
